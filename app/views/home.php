bd05f57497b3ada8dd38a03cb6462ab6e1a98ff6
Abbas Ali <abbas@ranium.in>
2014-07-08 11:49:23 +0530

    Removed the email helper method

--- app/views/home.php
+++ app/views/home.php
@@ -1,5 +1,5 @@
 <div class="jumbotron">
   <h1>Hello there!</h1>
-  <p>This is a good bootstrap app to get your project started.</p>
+  <p>This is a bootstrap app to get your project started.</p>
   <p>Edit <code>HomeController::index()</code> action to add functionality to the home page of your app. The view being used to render the home page is at <code>app/views/home.blade.php</code>.</p>
 </div>
